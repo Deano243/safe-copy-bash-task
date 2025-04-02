546d30950d776acfa78a700feba3176a74edaa28
deano243 <deanooz243@hotmail.com>
2025-04-02 19:24:31 +0300

    fixing my first version of the code

--- safecopy.sh
+++ safecopy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #validate the the file exist and if the file is writablei
 file_name=$1
-if [[  -e "$file_name" ]]; then
-	echo "file exist"
-elif [[ -w  "$file_name" ]]; then
-	echo "file is writable"
+if [[ ! -e "$file_name" ]]; then
+	echo "file is not exist"
+elif [[ ! -w  "$file_name" ]]; then
+	echo "file is exist and not writable"
 else
-	echo "file is not exist and not writable"
+	echo "file is exist and  writable"
 fi
