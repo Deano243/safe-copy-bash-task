189cdbb66a54e05bb118dca8b15cfb6282bdf261
deano243 <deanooz243@hotmail.com>
2025-04-02 19:42:04 +0300

    calculate the source file or dir size and calcule the destination file or dir available space

--- safecopy.sh
+++ safecopy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#validate the the file exist and if the file is writablei
-file_name=$1
-if [[ ! -e "$file_name" ]]; then
+#validate if the file is exist and if the file is writable
+source_name=$1
+destination_name=$2
+if [[ ! -e "$source_name" ]]; then
 	echo "file is not exist"
-elif [[ ! -w  "$file_name" ]]; then
+elif [[ ! -w  "$source_name" ]]; then
 	echo "file is exist and not writable"
 else
 	echo "file is exist and  writable"
 fi
+#calculating the size of the source file and the free space in the destination
+source_size=$(du -sh "$source_name")
+destination_space=$(df -h "$destination_name")
+echo "source size is $source_size and the available space in the destination is $destination_space"
