92568e2882b1722063938f414787eaa5d13328b9
deano243 <deanooz243@hotmail.com>
2025-04-02 21:30:31 +0300

    same as before just refining

--- safecopy.sh
+++ safecopy.sh
@@ -13,11 +13,19 @@ fi
 source_size=$(du -s "$source_name"| awk '{print 1}')
 destination_space=$(df --output=avail "$destination_name"| tail -1)
 #if space is sufficient display stats and ask user to confirm with the operation
+check_size() {
 if [[ "$source_size" -lt "$destination_space" ]]; then
 	echo "source size is $source_size KB and the available space in the destination is $destination_space KB"
-read -p "do you want to continue with the operation y/n?"
+read -p "do you want to continue with the operation y/n?" user_choice
+if [[ "$user_choice" == n ]]; then
+	echo "stop everything"
+	exit 1
+fi
 #if there is not sufficient space print that and delete old files"
 else
 	echo "there is not enough space"
 	find "$destination_name" -type f -mtime +30 -exec rm -f {} \;
 fi
+done
+}
+check_size
