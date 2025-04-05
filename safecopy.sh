3d109fddedc3c95370bae5887de4ba15b4f95389
deano243 <deanooz243@hotmail.com>
2025-04-06 02:23:52 +0300

    last version completing the task untill question 7

--- safecopy.sh
+++ safecopy.sh
@@ -13,7 +13,6 @@ fi
 source_size=$(du -s "$source_name"| awk '{print 1}')
 destination_space=$(df --output=avail "$destination_name"| tail -1)
 #if space is sufficient display stats and ask user to confirm with the operation
-check_size() {
 if [[ "$source_size" -lt "$destination_space" ]]; then
 	echo "source size is $source_size KB and the available space in the destination is $destination_space KB"
 read -p "do you want to continue with the operation y/n?" user_choice
@@ -25,7 +24,37 @@ fi
 else
 	echo "there is not enough space"
 	find "$destination_name" -type f -mtime +30 -exec rm -f {} \;
+	
 fi
-done
-}
-check_size
+#recalculate space after deletion
+echo "space after deletion is: $destination_space KB"
+
+if [[ "$source_size" -lt "$destination_space" ]]; then
+        echo "source size is $source_size KB and the available space in the destination is $destination_space KB"
+read -p "do you want to continue with the operation y/n?" user_choice
+if [[ "$user_choice" == n ]]; then
+        echo "stop everything"
+        exit 1
+fi
+else 
+	echo "there is not enough space"
+	read -p "do you want to retry cleanup? y/n" clean_choice
+	if [[ "$clean_choice" == "n" ]]; then
+		echo "operation cancelled"
+		exit 1
+	else
+		echo "please run the script again"
+		exit 1
+	fi
+fi
+fi
+#perform copy 
+cp -r "$source_name" "$destination_name"
+if [[ $? -eq 0 ]]; them
+	echo "succesful"
+else
+	echo "failed
+fi
+
+
+
